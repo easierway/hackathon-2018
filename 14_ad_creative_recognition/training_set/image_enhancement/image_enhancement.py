abb9a44dad12b88d6f9c4dec54ee2eaf78c208af
hatlonely <hatlonely@gmail.com>
2018-08-06 15:29:57 +0800

    update

--- 14_ad_creative_recognition/training_set/image_enhancement/image_enhancement.py
+++ 14_ad_creative_recognition/training_set/image_enhancement/image_enhancement.py
@@ -13,23 +13,29 @@ def combine(oimage, logo_path, eimage, scale):
     logo = cv2.imread(logo_path)
     logo = cv2.resize(logo, (0, 0), fx=scale, fy=scale,
                       interpolation=cv2.INTER_NEAREST)
+    # rotate
+    # lh, lw, _ = logo.shape
+    # rotate = cv2.getRotationMatrix2D(
+    #     (lw / 2, lh / 2), random.randint(0, 360), 1)
+    # logo = cv2.warpAffine(logo, rotate, (lw, lh))
+
     logo_gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
     _, logo_mask0 = cv2.threshold(
         logo_gray, 254, 255, cv2.THRESH_BINARY)
     logo_mask1 = cv2.bitwise_not(logo_mask0)
 
     img = cv2.imread(oimage, cv2.IMREAD_COLOR)
-    lrows, lcols, _ = logo.shape
-    irows, icols, _ = img.shape
-    x = random.randint(0, irows - lrows)
-    y = random.randint(0, icols - irows)
+    lh, lw, _ = logo.shape
+    ih, iw, _ = img.shape
+    x = random.randint(0, ih - lh)
+    y = random.randint(0, iw - lw)
 
-    cimg = img[x: x + lrows, y: y + lcols].copy()
+    cimg = img[x: x + lh, y: y + lw].copy()
     cimg_and = cv2.bitwise_and(cimg, cimg, mask=logo_mask0)
     logo_and = cv2.bitwise_and(logo, logo, mask=logo_mask1)
     cimg_logo = cv2.add(cimg_and, logo_and)
     cimg_logo = cv2.medianBlur(cimg_logo, 1)
-    img[x: x + lrows, y: y + lcols] = cimg_logo[:, :]
+    img[x: x + lh, y: y + lw] = cimg_logo[:, :]
     cv2.imwrite(eimage, img)
 
 
@@ -40,7 +46,7 @@ def main():
     parser.add_argument("-e", "--eimages", default="eimages",
                         help="enhancement images")
     parser.add_argument(
-        "-l", "--logo", default="logo/douyin.jpg", help="logo path")
+        "-l", "--logo", default="logo/douyin.png", help="logo path")
     args = parser.parse_args()
     for filename in os.listdir(args.oimages):
         print("enhance {}".format(filename))
