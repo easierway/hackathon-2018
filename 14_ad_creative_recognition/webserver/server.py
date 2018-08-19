2bf96de92c8b0806468b1b5797b2d047e49fae81
hatlonely <hatlonely@gmail.com>
2018-08-19 13:46:57 +0800

    update

--- 14_ad_creative_recognition/webserver/server.py
+++ 14_ad_creative_recognition/webserver/server.py
@@ -35,11 +35,11 @@ models = []
 
 def init_model():
     weights = [
-        ("700", "/home/ec2-user/src/wukong/tmp/douyin_700.top_weights.best.hdf5"),
-        ("672", "/home/ec2-user/src/wukong/tmp/douyin_672.combined_model_weightsacc0.938_val_acc0.948.best.hdf5"),
-        ("448", "/home/ec2-user/src/wukong/tmp/douyin_448.combined_model_weightsacc0.90_val_acc0.99.best.hdf5"),
-        ("300", "/home/ec2-user/src/wukong/tmp/douyin_300.combined_model_weightsacc0.85_val_acc0.96.best.hdf5"),
-        ("224", "/home/ec2-user/src/wukong/tmp/douyin_224.combined_model_weightsacc0.83_val_acc0.92.best.hdf5"),
+        (700, "/home/ec2-user/src/wukong/tmp/douyin_700.top_weights.best.hdf5"),
+        (672, "/home/ec2-user/src/wukong/tmp/douyin_672.combined_model_weightsacc0.938_val_acc0.948.best.hdf5"),
+        (448, "/home/ec2-user/src/wukong/tmp/douyin_448.combined_model_weightsacc0.90_val_acc0.99.best.hdf5"),
+        (300, "/home/ec2-user/src/wukong/tmp/douyin_300.combined_model_weightsacc0.85_val_acc0.96.best.hdf5"),
+        (224, "/home/ec2-user/src/wukong/tmp/douyin_224.combined_model_weightsacc0.83_val_acc0.92.best.hdf5"),
     ]
     for size, weight in weights:
         model = WuKongVisionModel(size, size)
